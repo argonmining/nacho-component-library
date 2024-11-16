f5499b8e8da11e733ebd916181ff578b5f836495
CrixusHD <nick.engl@gmx.de>
2024-11-16 23:10:03 +0100

    update

---
+++ lib/esm/index.d.ts
@@ -0,0 +1,5 @@
+export { ThemeToggle } from './components/themeToggle/ThemeToggle';
+export { MobileProvider, useMobile } from './hooks/mobile';
+export { DarkModeProvider, useDarkMode } from './hooks/darkMode';
+export { setLocalStorageItem, getLocalStorageItem } from './services/LocalStorageHelper';
+export { sendHeadRequest, sendRequest, simpleRequest } from './services/RequestService';
